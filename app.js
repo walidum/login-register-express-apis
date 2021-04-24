097d208c5341107f74f52038d35e234b3c36dede
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-04-24 08:50:47 +0100

    add password hashing

--- app.js
+++ app.js
@@ -11,5 +11,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('', api)
-const port = 3001
-app.listen(port, () => console.log(`app listening on port ${port}!`))
+app.get("/", function (req, res) {
+    res.send("<h1>Hello World!</h1>")
+})
+
+app.listen(process.env.PORT || 3000)
